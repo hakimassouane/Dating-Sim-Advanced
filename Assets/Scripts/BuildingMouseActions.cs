528d3eecab05c90d00d8e748b505dc3b81512ddb
Hakim Assouane <hakim.assouane@epitech.eu>
2019-06-16 04:32:49 +0200

    Basics of outline gameobject
    - Added outlinematerial assets, and git ignored them
    - Added scripts / shaders / material folders
    - Added BuildingMouseActions script

---
+++ Assets/Scripts/BuildingMouseActions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMouseActions : MonoBehaviour
+{
+      void OnMouseOver()
+     {
+         print (gameObject.name);
+     }
+}
