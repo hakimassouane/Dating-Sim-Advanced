ab22b3853690ee7832aae92cf791b0280c8a84ee
Hakim Assouane <hakim.assouane@epitech.eu>
2019-06-16 05:28:09 +0200

    Updated scene 2
    - Updated scene buildings
    - Updated script BuildingMouseAction to now be able to outline a building and change cursor when hovering over building

--- Assets/Scripts/BuildingMouseActions.cs
+++ Assets/Scripts/BuildingMouseActions.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class BuildingMouseActions : MonoBehaviour
 {
-      void OnMouseOver()
-     {
-         print (gameObject.name);
-     }
+	private Outline outscript;
+	public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+	void Start ()
+	{
+		outscript = gameObject.GetComponent<Outline>();
+	}
+
+    void OnMouseOver()
+    {
+    	Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    	outscript.OutlineWidth = 5;
+    }
+
+    void OnMouseExit()
+    {
+    	Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    	outscript.OutlineWidth = 0;
+    }
 }
