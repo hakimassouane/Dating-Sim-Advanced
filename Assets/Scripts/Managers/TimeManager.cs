c764384309142fa012b48e0cd29d2e9f4ad44b94
Hakim Assouane <hakim.assouane@epitech.eu>
2019-06-18 00:47:16 +0200

    Added Managers
    - Added Manager folder
    - Added TimeManager which is a singleton to handle time management in game
    - Added TravelManager which is a singleton to handle the travel management (only handling which was the last building you went in for now)

---
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+	private static TimeManager _instance;
+
+	public static TimeManager Instance {
+		get {
+			if (_instance == null) {
+				_instance = new TimeManager();
+			}
+			return _instance;
+		}
+	}
+
+	private static string[] months;
+
+	// Time variables
+	private static int currentYear = 2019;
+	private static int currentMonth = 8;
+	private static int currentDay = 1;
+	// in minutes
+	private static int currentTime = 420;
+
+
+	// Initialize method
+	void Start() {
+		months = new string[] {
+		"Janvier",
+		"Fevrier",
+		"Mars",
+		"Avril",
+		"Mai",
+		"Juin",
+		"Juillet",
+		"Aout",
+		"Septembre",
+		"Octobre",
+		"Novembre",
+		"Decembre"
+		};
+	}
+
+	public void addYear(int amount) {
+		currentYear += amount;
+		updateDateUI();
+	}
+
+	public void addMonth(int amount) {
+		int addedMonthCount = 0;
+
+		while (addedMonthCount < amount) {
+			addedMonthCount += 1;
+			if (currentMonth + 1 > 12) {
+				currentMonth = 0;
+				addYear(1);
+			}
+			currentMonth += 1;
+		}
+		updateDateUI();
+	}
+
+	public void addWeek(int amount) {
+		addDay(amount * 7);
+	}
+
+	public void addDay(int amount) {
+		int addedDaysCount = 0;
+		while (addedDaysCount < amount) {
+			addedDaysCount += 1;
+			if (currentDay + 1 > 30) {
+				currentDay = 0;
+				addMonth(1);
+	  	 	}
+		 	currentDay += 1;
+		}
+		updateDateUI();
+	}
+
+	public void addHours(int amount) {
+		int addedTimeCount = 0;
+		while (addedTimeCount < 60 * amount) {
+			addedTimeCount += 1;
+			if (currentTime + 1 > 1440) {
+				currentTime = 0;
+	  	 	}
+		 	currentTime += 1;
+		}
+		updateDateUI();
+	}
+
+	public void addMinutes(int amount) {
+		int addedTimeCount = 0;
+		while (addedTimeCount < amount) {
+			addedTimeCount += 1;
+			if (currentTime + 1 > 1440) {
+				currentTime = 0;
+				addDay(1);
+	  	 	}
+		 	currentTime += 1;
+		}
+		updateDateUI();
+	}
+
+
+	public void updateDateUI() {
+		GameObject dateUI = GameObject.Find("Date");
+		Text dateText = dateUI.GetComponent<Text>();
+		string dateStringTemplate = currentDay + " " + months[currentMonth] + " " + currentYear + " " + TimeSpan.FromMinutes(currentTime).ToString(@"hh\:mm");
+
+		dateText.text = dateStringTemplate;
+	}
+}
---
+++ Assets/Scripts/Managers/TravelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TravelManager : MonoBehaviour
+{
+	private static TravelManager _instance;
+
+	public static TravelManager Instance {
+		get {
+			if (_instance == null) {
+				_instance = new TravelManager();
+			}
+			return _instance;
+		}
+	}
+
+	public static GameObject lastVisitedBuilding = null;
+
+	public GameObject getLastVisitedBuilding() {
+		return lastVisitedBuilding;
+	}
+
+	public void setLastVisitedBuilding(GameObject newBuilding) {
+		lastVisitedBuilding = newBuilding;
+	}
+}
