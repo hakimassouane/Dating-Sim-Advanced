d2ab88412699b01fe1c17254af2b7b3047cc60f5
Hakim Assouane <hakim.assouane@epitech.eu>
2019-06-18 20:52:29 +0200

    Updated changeCameraPosition script
    - Added a lock to prevent multiple click and bugs
    - Updated time for lerp

--- Assets/Scripts/CameraScripts/ChangeCameraPosition.cs
+++ Assets/Scripts/CameraScripts/ChangeCameraPosition.cs
@@ -4,34 +4,46 @@ using UnityEngine;
 
 public class ChangeCameraPosition : MonoBehaviour
 {
+    private bool isLocked = false;
+
     public void moveLeft(int amount){
-    	StartCoroutine(smoothMoveLeft(amount, 1f));
+    	StartCoroutine(smoothMoveLeft(amount, 0.35f));
     }
 
     public void moveRight(int amount){
-    	StartCoroutine(smoothMoveRight(amount, 1f));
+    	StartCoroutine(smoothMoveRight(amount, 0.35f));
     }
 
     public IEnumerator smoothMoveLeft(int amount, float overTime)
 	{
-		Vector3 targetPosition = new Vector3(gameObject.transform.position.x - amount, gameObject.transform.position.y, gameObject.transform.position.z);
-    	float startTime = Time.time;
-    	while(Time.time < startTime + overTime)
-    	{
-        	gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, (Time.time - startTime)/overTime);
-        	yield return null;
-    	}
-    	gameObject.transform.position = targetPosition;
+        if (isLocked == false) {
+            isLocked = true;
+            Vector3 targetPosition = new Vector3(gameObject.transform.position.x - amount, gameObject.transform.position.y, gameObject.transform.position.z);
+            float startTime = Time.time;
+            while(Time.time < startTime + overTime)
+            {
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, (Time.time - startTime)/overTime);
+                yield return null;
+            }
+            gameObject.transform.position = targetPosition;
+            isLocked = false;
+        }
+		
 	}
 
     public IEnumerator smoothMoveRight(int amount, float overTime) {
-    	Vector3 targetPosition = new Vector3(gameObject.transform.position.x + amount, gameObject.transform.position.y, gameObject.transform.position.z);
-    	float startTime = Time.time;
-    	while(Time.time < startTime + overTime)
-    	{
-        	gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, (Time.time - startTime)/overTime);
-        	yield return null;
-    	}
-    	gameObject.transform.position = targetPosition;
+        if (isLocked == false) {
+            isLocked = true;
+            Vector3 targetPosition = new Vector3(gameObject.transform.position.x + amount, gameObject.transform.position.y, gameObject.transform.position.z);
+            float startTime = Time.time;
+            while(Time.time < startTime + overTime)
+            {
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, (Time.time - startTime)/overTime);
+                yield return null;
+            }
+            gameObject.transform.position = targetPosition;
+            isLocked = false;
+        }
+    	
     }
 }
