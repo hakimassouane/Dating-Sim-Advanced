1d4f15aa1a1cd9b9a511d365e2fd0cc4818fa4b3
Hakim Assouane <hakim.assouane@epitech.eu>
2019-06-20 11:42:36 +0200

    Added a car generator with moving vehicles
    - Added prefabs of working trucks
    - Created scripts to move vehicules
    - Updated gitignore file
    - Updated requiredAssets

---
+++ Assets/Scripts/Vehicules/CarGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGenerator : MonoBehaviour
+{
+	public GameObject[] carModel;
+	private float counter = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > 5) {
+        	generateCar();
+        	counter = 0;
+        }
+    }
+
+    void generateCar() {
+    	Instantiate(carModel[Random.Range(0, carModel.Length)], transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/Vehicules/MoveVehicule.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveVehicule : MonoBehaviour
+{
+	private Vector3 newPosition;
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+        startPosition = newPosition;
+        endPosition = new Vector3(startPosition.x + 250, startPosition.y, startPosition.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	print(startPosition);
+    	transform.position = Vector3.MoveTowards(startPosition, endPosition, 0.5f);
+    	startPosition = transform.position;
+    	if (transform.position == endPosition) {
+    		Destroy(gameObject);
+    	}
+    }
+}
