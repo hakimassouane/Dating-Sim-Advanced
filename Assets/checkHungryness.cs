9c2166978e83d90cd29221d987ad88e6cdd7d797
Hakim Assouane <hakim.assouane@epitech.eu>
2019-06-20 10:47:15 +0200

    Added tiredness and hungryness debuff indicators

---
+++ Assets/checkHungryness.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkHungryness : MonoBehaviour
+{
+	public GameObject background;
+	public GameObject icon;
+
+	private Image backgroundImage;
+	private Image iconImage;
+	private bool updateLock;
+
+	void Start() {
+		backgroundImage = background.GetComponent<Image>();
+		iconImage = icon.GetComponent<Image>();
+		updateLock = true;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StatManager.Instance.getStat("faim") == 0) {
+        	iconImage.enabled = true;
+        	backgroundImage.enabled = true;
+        	updateLock = false;
+        } 
+        else if (StatManager.Instance.getStat("faim") > 0 && updateLock == false) {
+        	print("yo");
+        	iconImage.enabled = false;
+        	backgroundImage.enabled = false;
+        	updateLock = true;
+        }
+    }
+}
