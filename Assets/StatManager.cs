2254b35e106f208142c5435f3c20882c20b2d59e
Hakim Assouane <hakim.assouane@epitech.eu>
2019-06-20 03:07:45 +0200

    Added a stat system
    - Updated StatManager script and linked to every slider bar
    - Updated slider and bars name in the ui of scene 2

--- Assets/StatManager.cs
+++ Assets/StatManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class StatManager : MonoBehaviour
@@ -16,19 +17,43 @@ public class StatManager : MonoBehaviour
 		}
 	}
 
-	Dictionary<string, int> statMap = new Dictionary<string, int>();
+	private static Slider strengthSlider;
+	private static Slider intelSlider;
+	private static Slider creaSlider;
+	private static Slider beautySlider;
+	private static Slider combatSlider;
+	private static Slider cultSlider;
+	private static Dictionary<string, int> statMap = new Dictionary<string, int>();
+	private static Dictionary<string, Slider> statSliderMap = new Dictionary<string, Slider>();
 
 	void Start() {
 		initStatMap();
+		initStatSliderMap();
 	}
 
 	private void initStatMap() {
-		statMap.Add("beaute", 0);
 		statMap.Add("force", 0);
 		statMap.Add("intelligence", 0);
-		statMap.Add("combat", 0);
 		statMap.Add("creativite", 0);
+		statMap.Add("beaute", 0);
+		statMap.Add("combat", 0);
 		statMap.Add("culture", 0);
+	}
+
+	private void initStatSliderMap() {
+		strengthSlider = GameObject.Find("strengthSlider").GetComponent<Slider>();
+		intelSlider = GameObject.Find("intelSlider").GetComponent<Slider>();
+		creaSlider = GameObject.Find("creaSlider").GetComponent<Slider>();
+		beautySlider = GameObject.Find("beautySlider").GetComponent<Slider>();
+		combatSlider = GameObject.Find("combatSlider").GetComponent<Slider>();
+		cultSlider = GameObject.Find("cultSlider").GetComponent<Slider>();
+
+		statSliderMap.Add("force", strengthSlider);
+		statSliderMap.Add("intelligence", intelSlider);
+		statSliderMap.Add("creativite", creaSlider);
+		statSliderMap.Add("beaute", beautySlider);
+		statSliderMap.Add("combat", combatSlider);
+		statSliderMap.Add("culture", cultSlider);
 	}
 
 	public void addStat(string statNameAndAmount) {
@@ -36,16 +61,23 @@ public class StatManager : MonoBehaviour
 		string name = split[0];
 		int amount = Int32.Parse(split[1]);
 
-		print(name + " " +  amount);
-
 		statMap[name] += amount;
-		
-		print(name + " is now : " + statMap[name]);
+		if (statMap[name] > 100) {
+			statMap[name] = 100;
+		}
+		else if (statMap[name] < 0) {
+			statMap[name] = 0;
+		}
+		updateStatUI(name);
 	}
 
+	public int getStat(string statName) {
+		return statMap[statName];
+	}
 
-
-	/* public int getStat(string statName) {
-
-	} */
+	public void updateStatUI(string statName) {
+		Slider currentSlider = statSliderMap[statName];
+		
+		currentSlider.value = statMap[statName];
+	}
 }
